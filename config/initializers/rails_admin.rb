f7f24b06d67e67aee5a37c35952a05863be5b11c
Davidson <davidsonts@outlook.com>
2020-09-26 01:04:22 -0300

    default items per page 25

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -22,7 +22,7 @@ RailsAdmin.config do |config|
   ## == Gravatar integration ==
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
-
+  config.default_items_per_page = 25
 
   config.model User do
     list do
